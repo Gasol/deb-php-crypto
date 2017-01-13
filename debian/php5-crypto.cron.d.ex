811cfc5043c228f24ef0118cb097ba1fd38f6e13
Gasol Wu <gasol.wu@gmail.com>
2017-01-13 18:00:14 +0800

    Remove *.ex, *.EX

--- debian/php5-crypto.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the php5-crypto package
-#
-0 4	* * *	root	[ -x /usr/bin/php5-crypto_maintenance ] && /usr/bin/php5-crypto_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libphp5-crypto 0.3.1 php5-crypto (>> 0.3.1-0), php5-crypto (<< 0.3.1-99)
